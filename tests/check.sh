0b1c608814484e31d1a9399fc42b4e12a3946193
Nicklaus McClendon <nicklaus@kulinacs.com>
2017-04-29 23:02:27 -0500

    Fixed tests

--- tests/check.sh
+++ tests/check.sh
@@ -2,7 +2,7 @@
 # Should be run from root directory of project
 
 # Lint Playbooks
-find . -type f -name "*.yml" | xargs yamllint
+find . -type f -name "*.yml" ! -path "*meta*" ! -path "*extra*" | xargs yamllint
 
 # Lint vars
  find *_vars -type f ! -iname vault | xargs yamllint
